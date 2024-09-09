7bc7d01ad9148a98358fa2b746595a5b72f0958a
ccorvino3 <c.corvino3@studenti.uniba.it>
2024-09-09 18:07:02 +0200

    Corretta procedura di apertura del programma

--- main.bat
+++ main.bat
@@ -2,8 +2,8 @@
 REM Cambia alla directory in cui si trova il file batch
 cd /d "%~dp0"
 
-REM Esegui il file compiler/main.py utilizzando l'interprete Python
-"%~dp0python.exe" main.py
+REM Esegui il file main.exe
+start main.exe
 
 REM Attendi un tasto per chiudere il terminale (opzionale)
 pause
